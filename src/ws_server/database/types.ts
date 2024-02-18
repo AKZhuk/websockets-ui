a68d1b12036203e5b1a7036fc87e71d24c1f8758
Aliaksei <aliaksei_zhuk@epam.com>
2024-02-18 19:04:20 +0100

    implement database

---
+++ src/ws_server/database/types.ts
@@ -0,0 +1,37 @@
+import { Coordinates, IndexPlayer, UserID } from "../types";
+
+export interface Users {
+  [key: UserID]: User;
+}
+export interface User {
+  name: string;
+  password: string;
+}
+
+export interface Room {
+  roomId: number;
+  roomUsers: UserID[];
+}
+
+export interface Game {
+  idGame: number;
+  playerIds: [UserID, UserID];
+  ships: [Ship[], Ship[]] | [];
+  pointsByShip: [{ [key: number]: any }, { [key: number]: any }];
+  turn: IndexPlayer;
+  shotHistory: [Coordinates[], Coordinates[]];
+}
+export interface Winner {
+  name: string;
+  wins: number;
+}
+
+export interface Ship {
+  position: {
+    x: number;
+    y: number;
+  };
+  direction: boolean;
+  length: number;
+  type: "small" | "medium" | "large" | "huge";
+}
